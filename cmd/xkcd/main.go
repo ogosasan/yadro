c1188997088c747f3833bc68e3dce47580d82a57
Egor <sorokin.egor04@mail.ru>
2024-04-10 22:39:16 +0300

    task3

--- cmd/xkcd/main.go
+++ cmd/xkcd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"yadro/internal/comics"
 	"yadro/internal/config"
 )
@@ -9,12 +10,17 @@ import (
 func main() {
 	var numComics int
 	var screen bool
-	flag.IntVar(&numComics, "n", 1234, "a int var")
+	flag.IntVar(&numComics, "n", -1, "a int var")
 	flag.BoolVar(&screen, "o", false, "a bool var")
 	flag.Parse()
 	var c config.Conf
 	c.GetConf()
 	baseURL := c.Url + "/%d/info.0.json"
+	numComics, err := GetNumComics()
+	if err != nil {
+		fmt.Println("Failed to get number of comics:", err)
+		return
+	}
 	comicsMap := comics.GoToSite(numComics, baseURL)
 	comics.WriteFile(screen, c.Bd, comicsMap)
 }
